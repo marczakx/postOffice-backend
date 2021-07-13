2562855d456c394db501f8dd4a024e82c2a05000
tom <marczakx@gmail.com>
2021-07-13 15:59:49 +0200

    Init

---
+++ src/main/java/marczakx/traditionalMail/TraditionalMailApplication.java
@@ -0,0 +1,13 @@
+package marczakx.traditionalMail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraditionalMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraditionalMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marczakx/traditionalMail/TraditionalMailApplicationTests.java
@@ -0,0 +1,13 @@
+package marczakx.traditionalMail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraditionalMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
