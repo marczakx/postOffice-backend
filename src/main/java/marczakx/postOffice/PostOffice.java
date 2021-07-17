1b5668bae86cdb311d5f687d21ff94d80831f1d6
tom <marczakx@gmail.com>
2021-07-17 23:00:56 +0200

    swagger

--- src/main/java/marczakx/postOffice/PostOffice.java
+++ src/main/java/marczakx/postOffice/PostOffice.java
@@ -2,12 +2,27 @@ package marczakx.postOffice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
 public class PostOffice {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PostOffice.class, args);
 	}
+	
+	@Bean
+	public Docket get() {
+		return new Docket(DocumentationType.SWAGGER_2).select().paths(PathSelectors.ant("/**"))
+				.apis(RequestHandlerSelectors.basePackage("marczakx.postOffice.web")).build();
+	}
 
 }
+
