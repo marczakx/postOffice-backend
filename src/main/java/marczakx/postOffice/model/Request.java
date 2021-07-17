b5bd86e5565e7139a2cfd5ea1edad57c4d1a2f83
tom <marczakx@gmail.com>
2021-07-17 13:04:08 +0200

    lombok

--- src/main/java/marczakx/postOffice/model/Request.java
+++ src/main/java/marczakx/postOffice/model/Request.java
@@ -7,7 +7,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Request {
 
 	@Id
@@ -17,52 +27,11 @@ public class Request {
 	private Priority priority;
 	private LocalDateTime time;
 
-	public Request() {
-	}
-
-	public Request(Long id, String pseudonym, Priority priority, LocalDateTime time) {
-		super();
-		this.id = id;
-		this.pseudonym = pseudonym;
-		this.priority = priority;
-		this.time = time;
-	}
-
 	public Request(String pseudonym, Priority priority, LocalDateTime time) {
 		this.pseudonym = pseudonym;
 		this.priority = priority;
 		this.time = time;
 	}
 
-	public Long getId() {
-		return id;
-	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getPseudonym() {
-		return pseudonym;
-	}
-
-	public void setPseudonym(String pseudonym) {
-		this.pseudonym = pseudonym;
-	}
-
-	public Priority getPriority() {
-		return priority;
-	}
-
-	public void setPriority(Priority priority) {
-		this.priority = priority;
-	}
-
-	public LocalDateTime getTime() {
-		return time;
-	}
-
-	public void setTime(LocalDateTime time) {
-		this.time = time;
-	}
 }
